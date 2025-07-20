d21c85a423709977b442991126edf911668fe7af
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-07-20 18:19:00 +0900

    Added resources

---
+++ src/main/java/com/github/TsutomuNakamura/resource_server_demo/ResourceServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.TsutomuNakamura.resource_server_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceServerDemoApplication.class, args);
+	}
+
+}
